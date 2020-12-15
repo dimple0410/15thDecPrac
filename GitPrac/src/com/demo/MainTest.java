4ebe1a555d1340401405254932ae6356b5ac19a5
dimpl <dimpl@LAPTOP-J2EAHDLS>
2020-12-15 16:48:28 +0530

    Added Scanner

--- GitPrac/src/com/demo/MainTest.java
+++ GitPrac/src/com/demo/MainTest.java
@@ -1,10 +1,15 @@
 package com.demo;
 
+import java.util.Scanner;
+
 public class MainTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number::");
+		int num=sc.nextInt();
+		
 	}
 
 }
